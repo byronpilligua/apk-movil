dd3cad79c6f4894ca1b47de62050d24676ef548f
PilliguaByron98 <pilliguabygre@gmail.com>
2022-05-13 23:36:02 -0500

    hola word

---
+++ app/src/main/java/com/example/citasmedicas/BASSPEDIDO.java
@@ -0,0 +1,19 @@
+package com.example.citasmedicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+hola=hola1=1;
+
+public class BASSPEDIDO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basspedido);
+
+
+        public getParentActivityIntent()
+    }
+}
